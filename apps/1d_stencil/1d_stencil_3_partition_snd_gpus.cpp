6caf82fea5d6fea526c92ef3ae5729363c52da38
Weile Wei <lokwei9@gmail.com>
2023-07-12 00:53:44 -0700

    add sender + gpus for partition version of stencil code

--- apps/1d_stencil/1d_stencil_3_partition_snd_gpus.cpp
+++ apps/1d_stencil/1d_stencil_3_partition_snd_gpus.cpp
@@ -11,6 +11,16 @@
 #include "commons.hpp"
 #include "argparse/argparse.hpp"
 
+#include <algorithm>
+#include <stdexec/execution.hpp>
+#include <exec/static_thread_pool.hpp>
+#include <exec/any_sender_of.hpp>
+#include <string>
+
+#include <nvexec/stream_context.cuh>
+
+#include <thrust/device_vector.h>
+
 // parameters
 struct args_params_t : public argparse::Args
 {
@@ -46,38 +56,8 @@ inline std::size_t idx(std::size_t i, int dir, std::size_t size)
 
 ///////////////////////////////////////////////////////////////////////////////
 // Our partition_data data type
-struct partition_data
-{
-    partition_data(std::size_t size = 0)
-      : data_(size)
-    {
-    }
+typedef thrust::device_vector<double> partition_data;
 
-    partition_data(std::size_t size, double initial_value)
-      : data_(size)
-    {
-        double base_value = double(initial_value * size);
-        for (std::size_t i = 0; i != size; ++i)
-            data_[i] = base_value + double(i);
-    }
-
-    double& operator[](std::size_t idx)
-    {
-        return data_[idx];
-    }
-    double operator[](std::size_t idx) const
-    {
-        return data_[idx];
-    }
-
-    std::size_t size() const
-    {
-        return data_.size();
-    }
-
-private:
-    std::vector<double> data_;
-};
 
 std::ostream& operator<<(std::ostream& os, partition_data const& c)
 {
@@ -98,64 +78,65 @@ struct stepper
     // Our data for one time step
     typedef partition_data partition;
     typedef std::vector<partition> space;
+    // typedef thrust::device_vector<partition> space;
 
     // Our operator
-    static double heat(double left, double middle, double right)
+    static double heat(double left, double middle, double right, const double k, const double dt, const double dx)
     {
         return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right);
     }
 
     // The partitioned operator, it invokes the heat operator above on all
     // elements of a partition.
-    static partition_data heat_part(partition_data const& left,
-        partition_data const& middle, partition_data const& right)
+    // static partition_data heat_part(partition_data& next, partition_data const& left,
+    static void heat_part(partition_data& next, partition_data const& left,
+        partition_data const& middle, partition_data const& right, const double k, const double dt, const double dx)
     {
         std::size_t size = middle.size();
-        partition_data next(size);
 
-        next[0] = heat(left[size - 1], middle[0], middle[1]);
+        next[0] = heat(left[size - 1], middle[0], middle[1], k, dt, dx);
 
         for (std::size_t i = 1; i != size - 1; ++i)
-            next[i] = heat(middle[i - 1], middle[i], middle[i + 1]);
+            next[i] = heat(middle[i - 1], middle[i], middle[i + 1], k, dt, dx);
 
-        next[size - 1] = heat(middle[size - 2], middle[size - 1], right[0]);
+        next[size - 1] = heat(middle[size - 2], middle[size - 1], right[0], k, dt, dx);
+    }
 
-        return next;
+    void init_value(space& data) {
+        for(std::size_t i = 0; i != data.size(); ++i) {
+            std::size_t nx = data[i].size();
+            double base_value = double(i * nx);
+            for(std::size_t j = 0; j != nx; ++j) {
+                data[i][j] = base_value + double(j);
+            }
+        }
     }
 
     // do all the work on 'np' partitions, 'nx' data points each, for 'nt'
     // time steps
-    space do_work(std::size_t np, std::size_t nx, std::size_t nt)
+    space do_work(stdexec::scheduler auto& sch, std::size_t np, std::size_t nx, std::size_t nt)
     {
-        // U[t][i] is the state of position i at time t.
-        std::vector<space> U(2);
-        for (space& s : U)
-            s.resize(np);
-
         // Initial conditions: f(0, i) = i
-        for (std::size_t i = 0; i != np; ++i)
-            U[0][i] = partition_data(nx, double(i));
-        
-        for (std::size_t i = 0; i != np; ++i) {
-            for(std::size_t j = 0; j != nx; ++j) {
-                std::cout << U[0][i][j] << " ";
-            }
-            std::cout << "\n";
-        }
+        space current(np, partition_data(nx));
+        space next(np, partition_data(nx));
+        init_value(current);
+        auto currentPtr = current.data();
+        auto nextPtr = next.data();
 
-        // Actual time step loop
-        for (std::size_t t = 0; t != nt; ++t)
-        {
-            space const& current = U[t % 2];
-            space& next = U[(t + 1) % 2];
+        stdexec::sender auto sender =
+            stdexec::schedule(sch) 
+            | stdexec::bulk(np, [=, k= ::k, dt = ::dt, dx = ::dx](int i) {
+                // Actual time step loop
+                for (std::size_t t = 0; t != nt; ++t) {
+                    heat_part(nextPtr[i], currentPtr[idx(i, -1, np)], currentPtr[i], currentPtr[idx(i, +1, np)],
+                        k, dt, dx);
+                    // swap current and next ptrs
+                    currentPtr[i].swap(nextPtr[i]);
+                }
+            });
 
-            for (std::size_t i = 0; i != np; ++i)
-                next[i] = heat_part(current[idx(i, -1, np)], current[i],
-                    current[idx(i, +1, np)]);
-        }
-
-        // Return the solution at time-step 'nt'.
-        return U[nt % 2];
+        stdexec::sync_wait(std::move(sender));
+        return current; 
     }
 };
 
@@ -173,19 +154,23 @@ int benchmark(args_params_t const & args)
     auto t = std::chrono::high_resolution_clock::now();
 
     // Execute nt time steps on nx grid points and print the final solution.
-    stepper::space solution = step.do_work(np, nx, nt);
+    nvexec::stream_context stream_ctx{};
+    stdexec::scheduler auto sch = stream_ctx.get_scheduler();
+    stepper::space solution = step.do_work(sch, np, nx, nt);
 
     auto elapsed = std::chrono::high_resolution_clock::now() - t;
 
     // Print the final solution
     if (args.results)
     {
-        for (std::size_t i = 0; i != nx; ++i)
-            std::cout << "U[" << i << "] = " << solution[i] << std::endl;
+        for (std::size_t i = 0; i != nx; ++i) {
+            std::cout << "U[" << i << "] = {"; 
+            for (std::size_t j = 0; j != solution[i].size(); ++j) {
+                std::cout << solution[i][j] << ", ";
+            }
+            std::cout << "}\n";
+        }
     }
-
-    // std::uint64_t const os_thread_count = hpx::get_os_thread_count();
-    // print_time_results(os_thread_count, elapsed, nx, np, nt, header);
 
     return 0; 
 }
